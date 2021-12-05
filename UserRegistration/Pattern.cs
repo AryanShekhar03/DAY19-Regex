559ae36dcda02d2b215fbf5bb084007431d8bf0e
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-06 00:14:38 +0530

    AddMobileNumber

--- UserRegistration/Pattern.cs
+++ UserRegistration/Pattern.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    class Pattern
+    public class Pattern
     {
+        public static void MobileNumber(string name)
+        {
+            string MobileNumber = "(0|91)?[ ][6-9][0-9]{9}";
+            Regex regex = new Regex(MobileNumber);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid number");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+
+        }
     }
 }
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,7 +6,11 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Regular Expression Program \n");
+            Console.WriteLine("Write your Mobile Number ex : +91 8447831498 .");
+            Console.Write("\n Enter Mobile Number : ");
+            string name = Console.ReadLine();
+            Pattern.MobileNumber(name);
         }
     }
 }
