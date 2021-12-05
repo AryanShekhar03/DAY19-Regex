9bdbee852063ef59b606b31aec5f6cdba461e580
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-05 21:51:52 +0530

    EnterLastNameInCapital

---
+++ UserRegistration2/Pattern.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration2
+{
+    class Pattern
+    {
+        public static void LastName(string name)
+        {
+            string LastName = "^[A-Z][a-zA-Z]{1,10}$"
+;
+
+            Regex regex = new Regex(LastName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter Last name start with capital letter and maximum 10 charactors");
+            }
+        }
+    }
+}
