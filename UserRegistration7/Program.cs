3ada92a1893bd89611777df0d0f3c0b6fb2134b1
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-06 14:09:40 +0530

    ShouldHaveOneNumericInPass

---
+++ UserRegistration7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
