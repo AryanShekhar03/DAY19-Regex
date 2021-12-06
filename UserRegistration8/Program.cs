0a7042edc5afe1dc27022737d9513da36ba2b918
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-06 14:14:30 +0530

    Have1splCharacter

---
+++ UserRegistration8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
